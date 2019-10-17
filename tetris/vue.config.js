6f8fa144b14018b00f60f215cc43255ddab7a14f
Max Froehlich <max@mfro.me>
2019-10-17 07:37:07 -0400

    Accommodate for ts-loader/thread-loader bug

--- tetris/vue.config.js
+++ tetris/vue.config.js
@@ -6,4 +6,22 @@ module.exports = {
             ignored: /node_modules/,
         },
     },
+    configureWebpack: cfg => {
+        if (process.env.NODE_ENV === 'production') {
+            let rule = cfg.module.rules.find(a => a.test.test('myfile.ts'));
+            let i = rule.use.findIndex(a => a.loader == 'thread-loader');
+            rule.use.splice(i, 1);
+        }
+    },
+    chainWebpack: cfg => {
+        if (process.env.NODE_ENV === 'production') {
+            cfg.module
+                .rule('ts')
+                .use('ts-loader')
+                .tap(options => {
+                    options.happyPackMode = false;
+                    return options
+                });
+        }
+    },
 };
